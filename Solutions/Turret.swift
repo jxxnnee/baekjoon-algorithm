05ed6830f06ace8e6a220a93843a8c4c308a1f72
KyungJun Min <q_alz@naver.com>
2025-02-26 14:38:28 +0900

    Update Turret.swift

--- Solutions/Turret.swift
+++ Solutions/Turret.swift
@@ -25,25 +25,31 @@ func solution(_ splited: [Int]) -> Int {
     let minus = Double(r2 - r1)
     
     // case 1 : 중점이 같으면서 반지름도 같을 경우
+    // 무한대에 수렴하기 때문에 -1을 리턴한다.
     if(x1 == x2 && y1 == y2 && r1 == r2) {
         return -1
     }
     // case 2-1: 두 원의 반지름 합보다 중점간 거리가 더 길 때
+    // 두 원이 서로 떨어져 있어 접점이 없으므로 0을 리턴한다.
     else if (distance > pow(sum, 2)) {
         return 0
     }
     // case 2-2: 원 안에 원이 있으나 내접하지 않을 때
+    // 큰 원 안에 작은 원이 떨어져 있는 형태로 겹쳐있는 형태이므로 접점이 없어 0을 리턴한다.
     else if (distance < pow(minus, 2)) {
         return 0
     }
     // case 3-1: 내접할 때
+    // 큰 원 안에 작은 원이 있지만 한 점이 붙어 있으므로 1을 리턴한다.
     else if (distance == pow(minus, 2)) {
         return 1
     }
     // case 3-2: 외접할 때
+    // 바깥에서 원 끼리 한 점이 붙어있는 형태이므로 1을 리턴한다.
     else if (distance == pow(sum, 2)) {
         return 1
     }
+    // 나머지 케이스는 모두 점 2개가 겹치는 형태이므로 2를 리턴한다.
     else {
         return 2
     }
