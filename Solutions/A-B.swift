1a889fa1f6dacea8aec22f25adcf36a7db1d37a9
KyungJun Min <q_alz@naver.com>
2025-02-26 14:30:19 +0900

    Update A-B.swift

--- Solutions/A-B.swift
+++ Solutions/A-B.swift
@@ -1,3 +1,10 @@
+//
+//  A-B.swift
+//  baekjoon-algorithm
+//
+//  Created by 민경준 on 2/26/25.
+//
+
 let input = readLine()!.split(separator: " ").compactMap { Int(String($0)) }
 
 let a = input[0]
